8e26bae3f11858659082a3de349f2b4e9cd0c475
bharathi-dot <bharathibharathi5323@gmail.com>
2024-09-22 16:50:00 -0700

    adding privacy-policy with google analytics

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
 
     //-----------------Show the popup when the page loads-----------------//
-    document.getElementById('disclaimer-popup').style.display = 'flex';
+    // document.getElementById('disclaimer-popup').style.display = 'flex';
 
-    // Enable the button when the checkbox is checked
-    document.getElementById('agree-checkbox').addEventListener('change', function () {
-        document.getElementById('agree-button').disabled = !this.checked;
-    });
+    // // Enable the button when the checkbox is checked
+    // document.getElementById('agree-checkbox').addEventListener('change', function () {
+    //     document.getElementById('agree-button').disabled = !this.checked;
+    // });
 
-    // Hide the popup when the user clicks "Continue"
-    document.getElementById('agree-button').addEventListener('click', function () {
-        document.getElementById('disclaimer-popup').style.display = 'none';
-    });
+    // // Hide the popup when the user clicks "Continue"
+    // document.getElementById('agree-button').addEventListener('click', function () {
+    //     document.getElementById('disclaimer-popup').style.display = 'none';
+    // });
     //=======================//========================//
 
     var apiEndPoint = "https://localhost:7156";
